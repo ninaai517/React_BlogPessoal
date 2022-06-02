1430ac836aaab99f53a63e3145f01bbaec88f61d
ninaai517 <carolina.aizawa.moreira@gmail.com>
2022-06-02 02:04:55 -0300

    criando pagina de login

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import { Grid, Typography, Button } from '@material-ui/core';
 import { Box } from '@mui/material';
 import './Home.css'; //Importando o arquivo css da pasta home
+import { PaddingOutlined } from '@mui/icons-material';
 
 function Home() { //um componente só retorna 1 elemento
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#f7f1e3" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "#fd79a8", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "#fd79a8", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3"  className='titulo'>Um blog para compartilhar origamis!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5"  className='titulo2'>Clique aqui para iniciar uma postagem</Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                        </Box>
-                        <Button variant="outlined" style={{ borderColor: "#fd79a8", backgroundColor: "#f7f1e3", color: "#fd79a8" }}>Ver Postagens</Button>
+                        <Box marginRight={1}></Box>
+                        <Button variant="outlined" className='botao'>Postar</Button>
                     </Box>
 
                 </Grid>
-                <Grid item xs={6} >
-                    <img src="https://i.pinimg.com/564x/bf/a4/d1/bfa4d1e5f3d4388f3ba865c21f537ee9.jpg" alt="" width="500px" height="500px" />
+                <Grid item xs={6}>
+                    <img src='https://github.com/ninaai517/React_BlogPessoal/blob/main/Assets/img/Kusudama-removebg-preview.png?raw=true' alt="" width="600x" height="500px" />
                 </Grid>
 
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} className='postagens'>
                 </Grid>
 
             </Grid>
